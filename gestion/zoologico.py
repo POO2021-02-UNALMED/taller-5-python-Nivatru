7c463a20e965707a09ef0e0bcf10b406fa16c5f9
Nicolás Valencia Trujillo <nvalenciat@unal.edu.co>
2021-11-23 22:59:59 -0500

    Update zoologico.py

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -22,7 +22,7 @@ class Zoologico:
     def setUbicacion(self, ubicacion):
         self._ubicacion = ubicacion
 
-    def getZonas(self):
+    def getZona(self):
         return self._zonas
 
     def setZonas(self, zonas):
