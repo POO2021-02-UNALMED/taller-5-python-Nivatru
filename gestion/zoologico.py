26c4d49041456d99d59d071e3a60ebdf315cd409
Nicolás Valencia Trujillo <nvalenciat@unal.edu.co>
2021-11-23 22:34:46 -0500

    a

---
+++ gestion/zoologico.py
@@ -0,0 +1,33 @@
+
+
+class Zoologico:
+
+    def __init__(self, nombre, ubicacion, zonas):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = zonas
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion
+
+    def getZonas(self):
+        return self._zonas
+
+    def setZonas(self, zonas):
+        self._zonas = zonas
+
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
+    def cantidadTotalAnimales(self):
+        animales = [x.cantidadAnimales() for x in self._zonas]
+        return sum(animales)
